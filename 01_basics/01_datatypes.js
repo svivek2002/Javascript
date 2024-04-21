22a448f9f6eda1e68f072255ce6bca7cc606a06a
Vivek Kumar Singh <97838886+svivek2002@users.noreply.github.com>
2024-04-21 08:05:06 +0000

    datatypes and variables

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+2) // will not run here, it needs JS engine, and we are node js not browser
+
+let name = "vivek"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 ^ 53
+// BigInt
+// String => ""
+// Boolean 
+// null => standalone value
+// undefined =>
+// Symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof "vivek")
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 1455656
+let accountEmail = "vivek123@gmail.com"
+var accountPassword = "14155"
+let accountState; // undefined
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+
+let is the block scoped variable
+Only work with let and const
+*/
+accountCity = "Delhi"
+
+// accountId = 2 // const can't be changed
+
+
+accountEmail = "vs@vs.com"
+accountPassword = "652265532"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.log(accountEmail);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
