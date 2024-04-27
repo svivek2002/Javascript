533df784813aa3f74040d3846334499aba2e63af
Vivek Kumar Singh <97838886+svivek2002@users.noreply.github.com>
2024-04-27 05:29:20 +0000

    Comparisons

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -2,6 +2,5 @@ console.log("02" > 1) // true
 console.log(null > 0) // false
 console.log(null == 0) //false
 console.log(null <= 0); //true OR console.log(null >= 0)
-
 console.log("2" == 2) // true as only value is checked
 console.log("2" === 2) // false as datatypes are also checked
