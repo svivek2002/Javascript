ef034cebfbb723de17a0cdc190f13ae9c424b7c2
Vivek Kumar Singh <97838886+svivek2002@users.noreply.github.com>
2024-05-18 06:39:23 +0000

    Functions Basics Part1

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,3 +34,48 @@ console.log(loginUserMessage("Vivek"));
 // console.log(loginUserMessage()); // undefined argument, so if block will execute 
 console.log(loginUserMessage()); // undefined argument, so default parameter will be used
 
+// function with not predefined number of parameters
+
+// ... is the rest and spread operator, they differ not in their usecases
+
+function calculateCartPrice(...price1){  // ... here is the rest operator
+    return price1
+}
+
+function calculateCartPrice1(num1, num2, ...price1){  // ... here is the rest operator
+    return price1
+}
+
+console.log(calculateCartPrice(200,400,500));
+console.log(calculateCartPrice1(200,500,600,1000)); // [ 600, 1000 ]
+
+// objects in functions
+
+const user = {
+    username : "vivek",
+    branch : "cse"
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and branch is ${anyobject.branch}`);
+}
+
+handleObject(user)
+// or you can directly pass objects in function as arguments
+
+handleObject({
+    username : "singh",
+    branch : "ECE"
+})
+
+// arrays in functions
+
+const myNewAArray = ['hero','villain','heroine','gangster']
+
+function handleArray(anyArray){
+    console.log(anyArray);
+}
+// handleArray(myNewAArray)
+// can directly pass array as function arguments
+handleArray(['hero','villain','heroine','gangster'])
+
